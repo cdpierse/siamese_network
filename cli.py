a54d66624e8427dc577bcbab56d0d7d79fa2aa83
Charles Pierse <charlespierse@gmail.com>
2020-04-11 01:35:52 +0100

    Add basic testing

--- cli.py
+++ cli.py
@@ -1,5 +1,8 @@
 import click
-from utils import get_identities, get_pairs
+from utils import get_identities, get_pairs, split_ids
+import random
+import numpy as np
+
 
 @click.group()
 def main():
@@ -12,9 +15,20 @@ def hello():
 
 
 @main.command()
-@click.option('--size', default=1000, help='size of the pair and non pairs file')
-def create_pair_file(size):
-    pass
+@click.option('--split_size', default=0.9, help='size of the train test split')
+def create_train_test_files(split_size):
+    # Method for creating train and test index files
+    # * should firstly get all the identities and according to a train/test split split them up
+    # into unique non crossover groups
+    # * from there I'll need to work out a way to create an even amount of pairs/non-pairs
+    #
+    unique_ids = (get_identities()).identity_num.unique()
+    train_size = round(len(unique_ids) * 0.9)
+    test_size = len(unique_ids) - train_size
+
+    test_ids = np.random.choice(unique_ids, test_size)
+    train_ids = list(set(unique_ids) - set(test_ids))
+    print(train_ids)
 
 
 if __name__ == "__main__":
@@ -22,4 +36,3 @@ if __name__ == "__main__":
     # df = get_identities()
     # pairs = get_pairs(df)
     # # print(len(pairs[pairs.values > 1]))
-    
--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+import numpy as np
 
 DATA_DIR = os.path.join("data", "celeba-dataset")
 
@@ -13,3 +14,15 @@ def get_identities():
 
 def get_pairs(df: pd.DataFrame):
     return df.identity_num.value_counts()
+
+
+def get_unique_ids(identity_df: pd.DataFrame) -> list:
+    return identity_df.identity_num.unique()
+
+
+def split_ids(ids: list, split_size) -> tuple:
+    train_size = round(len(ids) * 0.9)
+    test_size = len(ids) - train_size
+    test_ids = np.random.choice(ids, test_size)
+    train_ids = list(set(ids) - set(test_ids))
+    return train_ids, list(test_ids)
